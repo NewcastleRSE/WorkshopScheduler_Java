2fe1824dbc4f14e462f2ddf077ddb29dc46c5596
NeatuR <63652413+NeatuR@users.noreply.github.com>
2023-01-22 21:26:06 +0000

    read CSV with scanner

---
+++ tmp/Maven/src/main/java/CSVReader.java
@@ -0,0 +1,26 @@
+import java.io.File;
+import java.util.Scanner;
+import java.io.FileNotFoundException;
+public class CSVReader {
+  private void csvReaderMethod1(String filePath){
+    try {
+      Scanner scanner = new Scanner(new File(filePath));
+
+      scanner.useDelimiter(",");
+      while (scanner.hasNext()){
+        System.out.println(scanner.next()+ " ");
+      }
+
+      scanner.close();
+    } catch (FileNotFoundException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public static void main(String[] args) {
+    String filePath = new File("").getAbsolutePath() + File.separator + "/src/main/resources/lesson.csv";
+    CSVReader csvObj = new CSVReader();
+
+    csvObj.csvReaderMethod1(filePath);
+  }
+}
