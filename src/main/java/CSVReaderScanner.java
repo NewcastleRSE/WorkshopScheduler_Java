44be33253f9916e5f53c6f32d0bf8e365def40ab
NeatuR <63652413+NeatuR@users.noreply.github.com>
2023-02-04 20:25:43 +0000

    Output CSV to HTML file
    - add missing url in CSV
    - add test output file

--- src/main/java/CSVReaderScanner.java
+++ src/main/java/CSVReaderScanner.java
@@ -1,26 +1,90 @@
 import java.io.File;
 import java.util.Scanner;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;   // Import the FileWriter class
+import java.io.IOException;  // Import the IOException class to handle errors
+
 public class CSVReaderScanner {
-  private void csvReaderMethod1(String filePath){
+
+  public void csvReaderMethod1(String filePath){
+
+//    try {
+//      FileWriter myWriter = new FileWriter("filename.html");
+//      myWriter.write("This is a file");
+//      myWriter.close();
+//      System.out.println("Successfully wrote to the file.");
+//    } catch (IOException e) {
+//      System.out.println("An error occurred.");
+//      e.printStackTrace();
+//    }
+
     try {
       Scanner scanner = new Scanner(new File(filePath));
+      String test = new String("<table>" +
+          "    <tr>" +
+          "        <th>Duration</th>" +
+          "        <th>Index</th>" +
+          "        <th>Episode Name</th>" +
+          "        <th>URL</th>" +
+          "    </tr>\n"
+      );
 
-      scanner.useDelimiter(",");
+//      test += "Hello";
+
+
+      scanner.useDelimiter("\n");
       while (scanner.hasNext()){
-        System.out.println(scanner.next()+ " ");
+        String[] row = scanner.next().split(",");
+
+        test += "<tr>";
+
+//        test += scanner.next();
+
+        for(int i=0; i< row.length; i++)
+        {
+          test += "<td>";
+
+          if (row[i].contains("url")) {
+            test += "<a href='";
+            test += row[i];
+            test += "'>URL</a>";
+          } else {
+            test += row[i];
+          }
+
+          test += "</td>";
+        }
+
+        test += "</tr>\n";
+
+
+        // System.out.println(scanner.next()+ " ");
       }
+
+      test += "</table>";
+      // System.out.println(test);
+      FileWriter myWriter = new FileWriter("table.html");
+      myWriter.write(test);
+      myWriter.close();
+      System.out.println("Successfully wrote to the file.");
 
       scanner.close();
     } catch (FileNotFoundException e) {
       e.printStackTrace();
+    } catch (IOException e) {
+      throw new RuntimeException(e);
     }
   }
 
   public static void main(String[] args) {
-    String filePath = new File("").getAbsolutePath() + File.separator + "data/lesson.csv";
+    String filePath = new File("").getAbsolutePath() + File.separator + "/data/lesson.csv";
     CSVReaderScanner csvObj = new CSVReaderScanner();
 
     csvObj.csvReaderMethod1(filePath);
+
   }
+
 }
+
+
+
