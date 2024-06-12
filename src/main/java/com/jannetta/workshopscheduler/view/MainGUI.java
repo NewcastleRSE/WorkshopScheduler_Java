f47037fc2c54f41d3673f37ad82064467223d60c
Jannetta Steyn <6432530+jsteyn@users.noreply.github.com>
2024-06-12 22:28:41 +0100

    Update version number

--- src/main/java/com/jannetta/workshopscheduler/view/MainGUI.java
+++ src/main/java/com/jannetta/workshopscheduler/view/MainGUI.java
@@ -41,7 +41,7 @@ public class MainGUI extends JFrame {
             System.out.println("favicon.png not found.");
         }
 
-        JTextArea aboutLabel = new JTextArea("Version: 1.2\nAuthors:\nJannetta S. Steyn (GitHub: @jsteyn)\nRuxandra Neatu (GitHub: @NeatuR)\n" +
+        JTextArea aboutLabel = new JTextArea("Version: 1.3\nAuthors:\nJannetta S. Steyn (GitHub: @jsteyn)\nRuxandra Neatu (GitHub: @NeatuR)\n" +
                 "\nWorkshopScheduler takes a CSV file \n" +
                 "which consists of four columns,\n" +
                 "duration, Episode name, a summary \n" +
